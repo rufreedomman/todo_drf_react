c0622d6904960d831465323928617dd4dda18270
rufreedomman <rufreedomman@gmail.com>
2022-05-12 12:53:11 +0300

    initial commit

---
+++ backend/todo/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Todos'
+
+    def __str__(self):
+        return self.title
